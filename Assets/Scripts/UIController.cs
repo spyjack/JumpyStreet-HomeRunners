09824640085986572551a03708226d953fb098bd
krbrady <krbrady@my.waketech.edu>
2020-09-21 15:24:18 -0400

    Submitted main menu

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class UIController : MonoBehaviour
+{
+    public GameObject helpMenu;
+    public GameObject creditsMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnStartButtonClick()
+        {
+        print("SceneManager.LoadScene('Level1');");
+        }
+
+    public void OnHelpButtonClick()
+    {
+        helpMenu.SetActive(true);
+    }
+    public void OnCreditsButtonClick()
+    {
+        creditsMenu.SetActive(true);
+    }
+
+    public void OnCloseButtonClick()
+    {
+        helpMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+    }
+
+    public void OnQuitButtonClick()
+    {
+        print("Quitting");
+        Application.Quit();
+    }
+
+
+}
