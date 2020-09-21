cdd29e2c7107520461d025d7253cb4adcfc2ed4d
Terran <terranorion@gmail.com>
2020-09-20 21:11:08 -0400

    Added the resources

---
+++ Assets/Scripts/WorldGenerationController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerationController : MonoBehaviour
+{
+    [SerializeField]
+    private int worldHeightMax = 10;
+
+    [SerializeField]
+    private RowType[] worldRows;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InitializeWorld()
+    {
+        worldRows = new RowType[worldHeightMax];
+    }
+}
+
+public enum RowType
+{
+    street,
+    road,
+    river,
+    railroad,
+    grass,
+    forest
+}
